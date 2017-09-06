81129018619e9c6b6a317f11b1fd0c0f43533420
ashanaakh <ashanaakh@gmail.com>
2017-09-06 14:25:01 +0300

    Initial commit

---
+++ src/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+h1 {
+  color: green;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Hello from './Hello.vue'
+
+new Vue({
+  el: '#app',
+  components: {
+    hello: Hello
+  }
+})
